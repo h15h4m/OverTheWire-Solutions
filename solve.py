2a62fdb7bc635a3420a92964275ee02efc288f27
Hisham Alshaikh <30352665+h15h4m@users.noreply.github.com>
2017-07-22 19:54:43 +0300

    Update solve.py

--- solve.py
+++ solve.py
@@ -3,20 +3,15 @@
 
 import socket
 
-
 current_pass =  "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ";
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 
 s.connect( ("localhost", 30002 ));
 
 print s.recv(1024)
 
-
-
 f = open("result", 'w')
-
 
 i = 0
 
